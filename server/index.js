5276d7127468ab9115c67d6f357bb7266d02eed5
jtndr99 <jitmutreja20@gmail.com>
2024-05-26 17:37:20 +0530

    feat: user and post routes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose=require('mongoose')
 const dotenv=require('dotenv');
+const cookieParser=require('cookie-parser')
 const authRoute=require('./routes/auth.js');
+const userRoute=require('./routes/users.js');
+const postRoute=require('./routes/posts.js');
+const commentRoute=require('./routes/comments.js');
+
 
 
 dotenv.config();
 app.use(express.json())
+app.use(cookieParser());
 app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/comments",commentRoute)
+
+
 
 const connectDB=async()=>{
     try{
