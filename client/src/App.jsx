f174ca56a7c635c9b1ce46b017f78b7de7b052eb
jtndr99 <jitmutreja20@gmail.com>
2024-05-27 01:25:38 +0530

    feat: Integrated backend login/signup with frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import { UserContextProvider } from './context/UserContext'
 
 
 const App = () => {
@@ -10,11 +11,13 @@ const App = () => {
 
   
   return (
+    <UserContextProvider>
       <Routes>
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Register/>}/>
       </Routes>
+      </UserContextProvider>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose=require('mongoose')
 const dotenv=require('dotenv');
+const cors=require('cors')
+const multer=require('multer')
+const path=require("path")
 const cookieParser=require('cookie-parser')
 const authRoute=require('./routes/auth.js');
 const userRoute=require('./routes/users.js');
@@ -13,11 +16,28 @@ const commentRoute=require('./routes/comments.js');
 dotenv.config();
 app.use(express.json())
 app.use(cookieParser());
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
 app.use("/api/comments",commentRoute)
 
+
+const storage=multer.diskStorage({
+    destination:(req,file,fn)=>{
+        fn(null,"images")
+    },
+    filename:(req,file,fn)=>{
+        fn(null,req.body.img)
+    }
+})
+
+
+const upload=multer({storage:storage})
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.status(200).json("Image has been uploaded successfully!")
+})
 
 
 const connectDB=async()=>{
